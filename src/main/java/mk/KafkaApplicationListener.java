25ef24f7fe43fa3252bbd547fe38afd6a57242a3
marcin <marcin.kasinski@gmail.com>
2018-04-16 11:57:55 +0200

    removed spring-boot-properties-migrator

--- src/main/java/mk/KafkaApplicationListener.java
+++ src/main/java/mk/KafkaApplicationListener.java
@@ -79,7 +79,7 @@ public class KafkaApplicationListener {
 	public static void main(String[] args) {
 
 
-		System.out.println("Version 16.04.2018 (added join span)");
+		System.out.println("Version 16.04.2018 (removed spring-boot-properties-migrator)");
 		System.out.println("Environment variables");
 		
 
