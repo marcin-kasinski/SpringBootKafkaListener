a646be90e41680eee540b0fd2ba581d16408067b
marcin <marcin.kasinski@gmail.com>
2018-11-05 15:59:05 +0100

    remove filebeat start

--- docker/start.sh
+++ docker/start.sh
@@ -2,5 +2,5 @@
 
 #ls -l /
 #cat /etc/init.d/filebeat
-/etc/init.d/filebeat start
+#/etc/init.d/filebeat start
 java -jar -Dspring.profiles.active=$SPRING_PROFILE SpringBootKafkaListener-0.0.1-SNAPSHOT.jar
